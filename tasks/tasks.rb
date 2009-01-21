ddd4e61d30e1c4b766b8cc54af790c340ea707f6
Luke Melia <luke@lukemelia.com>
2009-01-21 01:31:57 -0500

    Add a rake task jobs:enqueue to enqueue a delayed_job by inserting into the jobs DB table without loading your entire Rails environment

--- tasks/tasks.rb
+++ tasks/tasks.rb
@@ -12,4 +12,23 @@ namespace :jobs do
   task :work => [:merb_env, :environment] do
     Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY']).start
   end
+  
+  desc "Enqueue a delayed_job by inserting into the jobs DB table without loading your entire Rails environment."
+  task :enqueue do
+    ENV['RAILS_ENV'] ||= 'development'
+    raise 'Usage: rake jobs:add METHOD=SomeClass.do_something RAILS_ENV=development' unless ENV['METHOD']
+    
+    $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
+    require "delayed_job"
+    
+    db = ENV['RAILS_ENV']
+    configurations = YAML.load_file(File.join(File.dirname(__FILE__), '../../../../config', 'database.yml'))
+    raise "no configuration for '#{db}'" unless configurations.key? db
+    configuration = configurations[db]
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+    ActiveRecord::Base.establish_connection(configuration)
+
+    Delayed::Job.enqueue Delayed::EvaledJob.new { ENV['METHOD'] }
+  end
+  
 end
