953e6a672b4492943204726acabd22ce6947b7b3
Bryan Helmkamp <bryan@brynary.com>
2009-05-19 11:51:14 -0400

    Switch specs to running on MySQL

--- spec/database.rb
+++ spec/database.rb
@@ -9,28 +9,33 @@ require File.dirname(__FILE__) + '/../init'
 require 'spec'
 
 ActiveRecord::Base.logger = Logger.new('/tmp/dj.log')
-ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => '/tmp/jobs.sqlite')
+ActiveRecord::Base.establish_connection({
+  :adapter  => 'mysql',
+  :username => 'root',
+  :encoding => 'utf8',
+  :database => 'delayed_job_test',
+})
 ActiveRecord::Migration.verbose = false
 
 sql = <<-SQL
-  DROP TABLE IF EXISTS "delayed_jobs";
-  CREATE TABLE "delayed_jobs" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
-    "priority"   INTEGER NOT NULL DEFAULT 0,
-    "attempts"   TINYINT NOT NULL DEFAULT 0,
-    "handler"    TEXT,
-    "last_error" MEDIUMTEXT,
-    "run_at"     DATETIME,
-    "locked_at"  DATETIME,
-    "locked_by"  CHAR(20),
-    "created_at" DATETIME,
-    "updated_at" DATETIME
+  DROP TABLE IF EXISTS `delayed_jobs`;
+  CREATE TABLE `delayed_jobs` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `priority`   INTEGER NOT NULL DEFAULT 0,
+    `attempts`   TINYINT NOT NULL DEFAULT 0,
+    `handler`    TEXT,
+    `last_error` MEDIUMTEXT,
+    `run_at`     DATETIME,
+    `locked_at`  DATETIME,
+    `locked_by`  CHAR(20),
+    `created_at` DATETIME,
+    `updated_at` DATETIME
   );
 
-  DROP TABLE IF EXISTS "stories";
-  CREATE TABLE "stories" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
-    "text" VARCHAR(255)
+  DROP TABLE IF EXISTS `stories`;
+  CREATE TABLE `stories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `text` VARCHAR(255)
   );
 SQL
 
